4b7861122e31601674a3117a81dc526fd0e0f408
earslaner2001 <earslaner2001@hotmail.com>
2025-09-28 18:00:07 +0300

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Botun gecikmesini ölçer.'),
+
+    async execute(interaction) {
+        const sent = await interaction.reply({ content: 'Pong!', fetchReply: true });
+        const ping = sent.createdTimestamp - interaction.createdTimestamp;
+        await interaction.editReply(`🏓 Pong! Bot gecikmesi: **${ping}ms** | API gecikmesi: **${interaction.client.ws.ping}ms**`);
+    },
+};
